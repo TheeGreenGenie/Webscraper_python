76470d90a1a846d0688ff466b74cfd4ff3b8d3f8
TheeGreenGenie <solomonshasanmi@gmail.com>
2025-04-08 14:48:02 -0700

    My updates

---
+++ web_scraper_project/scraper_app/scraper.py
@@ -0,0 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+
+def fetch_data(url):
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        return soup
+    else:
+        return None
