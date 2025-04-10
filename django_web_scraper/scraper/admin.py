67a5e33d0d3846599f77b4352fa6c6da2d4e1fa3
TheeGreenGenie <solomonshasanmi@gmail.com>
2025-04-10 10:11:31 -0700

    admin.py update

--- django_web_scraper/scraper/admin.py
+++ django_web_scraper/scraper/admin.py
@@ -1,3 +1,38 @@
 from django.contrib import admin
+from .models import ScrapedWebsite, ScrapedImage
 
-# Register your models here.
+class ScrapedImageInline(admin.TabularInline):
+    model = ScrapedImage
+    extra = 0
+    fields = ('image_url', 'alt_text', 'filename')
+    readonly_fields = ('image_url', 'alt_text', 'filename')
+    can_delete = False
+    max_num = 0
+    
+    def has_add_permission(self, request, obj=None):
+        return False
+
+@admin.register(ScrapedWebsite)
+class ScrapedWebsiteAdmin(admin.ModelAdmin):
+    list_display = ('url', 'title', 'scraped_at')
+    search_fields = ('url', 'title', 'description')
+    readonly_fields = ('url', 'title', 'description', 'keywords', 'author', 'scraped_at')
+    inlines = [ScrapedImageInline]
+    
+    def has_add_permission(self, request):
+        return False
+    
+    def has_change_permission(self, request, obj=None):
+        return False
+
+@admin.register(ScrapedImage)
+class ScrapedImageAdmin(admin.ModelAdmin):
+    list_display = ('image_url', 'website', 'filename')
+    search_fields = ('image_url', 'filename', 'alt_text')
+    readonly_fields = ('website', 'image_url', 'alt_text', 'filename')
+    
+    def has_add_permission(self, request):
+        return False
+    
+    def has_change_permission(self, request, obj=None):
+        return False
